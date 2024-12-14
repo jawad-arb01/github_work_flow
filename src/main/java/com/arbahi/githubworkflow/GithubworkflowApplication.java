0edd578f863a236251a81318f5da2af0a1d4a0d2
ARBAHI Jawad <106961582+jawad-arb@users.noreply.github.com>
2024-12-14 12:20:45 +0100

    added the workflow dispatch

---
+++ src/main/java/com/arbahi/githubworkflow/GithubworkflowApplication.java
@@ -0,0 +1,13 @@
+package com.arbahi.githubworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubworkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubworkflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arbahi/githubworkflow/GithubworkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arbahi.githubworkflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubworkflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
