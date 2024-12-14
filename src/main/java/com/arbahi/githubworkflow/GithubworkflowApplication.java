5cf7367347e865402cbe8e414a22d37fa84911f4
ARBAHI Jawad <106961582+jawad-arb@users.noreply.github.com>
2024-12-14 12:30:14 +0100

    update in the develop branch

--- src/main/java/com/arbahi/githubworkflow/GithubworkflowApplication.java
+++ src/main/java/com/arbahi/githubworkflow/GithubworkflowApplication.java
@@ -2,12 +2,19 @@ package com.arbahi.githubworkflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class GithubworkflowApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GithubworkflowApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
